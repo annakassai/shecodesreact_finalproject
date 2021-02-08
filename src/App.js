b674ff51ebe6f46592ddd88e73d6171bda36051a
annakassai <74617784+annakassai@users.noreply.github.com>
2021-02-08 19:01:46 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
-      <div className="footer-container">
-      <small className="link">
-        <a className="github-link"
-           href="https://github.com/annakassai/shecodesreact_finalproject"
-           target="_blank"
-           rel="noreferrer"
-           >
-           Open-source code</a>, by <a
-           className="linkedin-link"
-           href="https://www.linkedin.com/in/annakassai91/"
-           target="_blank"
-           rel="noreferrer"
-           >
-           Anna Kassai
-          </a>
-      </small>
+      <div className="container">
+      <h1>Weather App</h1>
+        <div className="footer-container">
+        <small className="link">
+          <a className="github-link"
+            href="https://github.com/annakassai/shecodesreact_finalproject"
+            target="_blank"
+            rel="noreferrer"
+            >
+            Open-source code</a>, by <a
+            className="linkedin-link"
+            href="https://www.linkedin.com/in/annakassai91/"
+            target="_blank"
+            rel="noreferrer"
+            >
+            Anna Kassai
+            </a>
+        </small>
+      </div>
     </div>
   </div>  
   );
