1eea3b9c647da4c3b78f8a847633e4ef306d6a8d
annakassai <74617784+annakassai@users.noreply.github.com>
2021-02-08 18:33:46 +0100

    Added footer and Git Url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <div className="footer-container">
+      <small className="link">
+        <a className="github-link"
+           href="https://github.com/annakassai/shecodesreact_finalproject"
+           target="_blank"
+           rel="noreferrer"
+           >
+           Open-source code</a>, by <a
+           className="linkedin-link"
+           href="https://www.linkedin.com/in/annakassai91/"
+           target="_blank"
+           rel="noreferrer"
+           >
+           Anna Kassai
+          </a>
+      </small>
     </div>
+  </div>  
   );
 }
 
