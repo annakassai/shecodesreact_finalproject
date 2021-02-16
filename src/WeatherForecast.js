bf68a33e21f5c437293614bfb8176d9c8ebf12f8
annakassai <74617784+annakassai@users.noreply.github.com>
2021-02-16 23:28:36 +0100

    Changed the number of forecasted slots

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,7 +15,7 @@ export default function WeatherForecast(props){
     if (loaded && props.city === forecast.city.name) {
       return(
         <div className="WeatherForecast row">
-          {forecast.list.slice(0, 6).map(function (forecastItem) {
+          {forecast.list.slice(0, 5).map(function (forecastItem) {
           return <WeatherForecastPreview data={forecastItem} />;
           })}
         </div>
